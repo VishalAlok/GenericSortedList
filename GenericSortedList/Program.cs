7a20fe431964c62aa994a3b7e60743687f640d69
vishal.Alok <you@example.com>
2021-10-15 06:38:36 +0530

    Add project files.

---
+++ GenericSortedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericSortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<string,string> obj = new SortedList<string,string>();
+            obj.Add("Vishal", "Bangalore");
+            obj.Add("Abhay", "Chennai");
+            obj.Add("Rahul", "Hyderabad");
+            obj.Add("Zak", "Pune");
+            foreach (string s in obj.Keys)
+            {
+                Console.Write(s + " " + obj[s] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
